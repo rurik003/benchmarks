a4d21bae4284883e2817d36b1a92ce5b2f6017f4
Will Rurik <rurik003@umn.edu>
2019-06-09 21:57:33 -0500

    added multithreading

--- reverse_comp.cpp
+++ reverse_comp.cpp
@@ -2,6 +2,8 @@
 #include <array>
 #include <vector>
 #include <memory>
+#include <thread>
+#include <functional>
 
 #include <sys/stat.h>
 #include <unistd.h>
@@ -23,47 +25,81 @@ constexpr std::array<char, 128> make_LUT()
 }
 
 constexpr auto lut = make_LUT();
+static const auto seq_len = 61;
+
+void reverse_comp_offset(char* start, char* end, const int offset, int num_rows)
+{
+	int rows = 0;
+	while (rows < num_rows) {
+		for (int i = 1; (i < offset); ++i) {
+			*start = lut[*start];
+			*end = lut[*end];
+			std::swap(*start, *end);
+			++start;
+			--end;
+		}
+		--end;
+		for (int i = 0; (i < (seq_len - offset)); ++i) {
+			*start = lut[*start];
+			*end = lut[*end];
+			std::swap(*start, *end);
+			++start;
+			--end;
+		}
+		++start;
+		++rows;
+	}
+}
+
+void reverse_comp_offset_final(char* start, char* end, const int offset)
+{
+	while (start < end) {
+		for (int i = 1; (i < offset) && (start < end); ++i) {
+			*start = lut[*start];
+			*end = lut[*end];
+			std::swap(*start, *end);
+			++start;
+			--end;
+		}
+		--end;
+		for (int i = 0; (i < (seq_len - offset)) && (start < end); ++i) {
+			*start = lut[*start];
+			*end = lut[*end];
+			std::swap(*start, *end);
+			++start;
+			--end;
+		}
+		++start;
+	}
+}
 
 // perform reverse complement
 // skip over new line characters by traversing data in strides
 // to eliminate branching
 void reverse_comp(char* start, char* end)
 {
-	static const auto seq_len = 61;
-	const auto offset = (end - start) % seq_len;
+	static auto hwconcurrency = std::thread::hardware_concurrency();
+	static auto nthreads = (hwconcurrency == 0) ? 4 : hwconcurrency;
+	const auto nbytes = end - start;
+	const auto last_row = nbytes % seq_len;
+	const auto offset = (last_row) == 0 ? seq_len : (last_row);
+	const auto nrows = nbytes / seq_len;
+	const auto nswaps = nrows >> 1;
+	
+	auto rows_per_thread = std::vector<unsigned int>(nthreads, nswaps / nthreads);
+	auto rem_rows = nswaps % nthreads;
+	for (int i = 0; rem_rows > 0; ++i, --rem_rows)
+		++rows_per_thread[i];
 	end -= 2;
-	if (offset) {
-		while (start < end) {
-			for (int i = 1; (i < offset) && (start < end); ++i) {
-				*start = lut[*start];
-				*end = lut[*end];
-				std::swap(*start, *end);
-				++start;
-				--end;
-			}
-			--end;
-			for (int i = 0; i < (seq_len - offset) && (start < end); ++i) {
-				*start = lut[*start];
-				*end = lut[*end];
-				std::swap(*start, *end);
-				++start;
-				--end;
-			}
-			++start;
-		}
-	} else {
-		while (start < end) {
-			for (int i = 1; i < seq_len; ++i) {
-				*start = lut[*start];
-				*end = lut[*end];
-				std::swap(*start, *end);
-				++start;
-				--end;
-			}
-			++start;
-			--end;
-		}
+	unsigned int strides = 0;
+	std::vector<std::thread> threads;
+	for (unsigned int i = 0; i < (nthreads - 1); ++i) {
+		threads.push_back(std::thread(reverse_comp_offset, start + strides, end - strides, offset, rows_per_thread[i]));
+		strides += rows_per_thread[i] * seq_len;
 	}
+	threads.push_back(std::thread(reverse_comp_offset_final, start + strides, end - strides, offset));
+	for (auto& t : threads)
+		t.join();
 }
 
 int main()
@@ -81,10 +117,8 @@ int main()
 	auto input = std::unique_ptr<char>(new char[len + 1]());
 	std::cin.read(input.get(), len);
 	
-	// find location of > to denote new entry
-	char* arrow = input.get();
-	char* end = input.get() + len + 1;
-	for (;
+	// find locations of > to denote new entry
+	for (char* arrow = input.get(), *end = input.get() + len + 1;
 	(arrow = reinterpret_cast<char*>(memchr(arrow, '>', end - arrow))) != nullptr;
 	++arrow) {
 		breakpoints.emplace_back(arrow);
